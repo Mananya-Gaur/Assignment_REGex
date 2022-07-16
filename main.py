5e7cac376b9a529ef35ae92b95df708581cc974a
Mananya <mananya.gaur5@gmail.com>
2022-07-16 13:36:09 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+name=input("enter your name")
+college=input("enter your college name")
+sem=input("enter your sem")
+data='{name} {college} {sem}'.format(name=name, college=college, sem=sem)
+print(data)
